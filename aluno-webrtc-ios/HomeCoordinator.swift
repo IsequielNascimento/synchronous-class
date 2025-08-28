54e5f6ab09d301dc66c44e2efc8417b7538e1014
IsequielNascimento <isequielnascimento@gmail.com>
2025-08-28 09:54:57 -0300

    update folders structure

---
+++ aluno-webrtc-ios/HomeCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  HomeCoordinator.swift
+//  aluno-webrtc-ios
+//
+//  Created by Isequiel Henrique do Nascimento on 28/08/25.
+//
+
+import Foundation
---
+++ aluno-webrtc-ios/aluno-webrtc-ios/Shared/App/aluno_webrtc_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  aluno_webrtc_iosApp.swift
+//  aluno-webrtc-ios
+//
+//  Created by Isequiel Henrique do Nascimento on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct aluno_webrtc_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeScreen()
+        }
+    }
+}
